4feb5f2760a4f44be29afa92c55d04982d8b080d
Juan Ovalle <jpovalleg@gmail.com>
2025-02-27 19:02:05 -0500

    Added prometheus

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from fastapi import FastAPI
 from config.db import init_db
 from router.router import match
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"]
+    allow_origins=["*"]
 )
 init_db()
 
+instrumentator = Instrumentator(should_group_status_codes=False).instrument(app)
+instrumentator.expose(app, endpoint="/metrics")
+
 app.include_router(match)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Match API"}
